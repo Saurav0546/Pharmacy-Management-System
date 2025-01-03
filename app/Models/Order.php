194e11f15eb8f715ed60479b847385f58995d059
Saurav <saurav3249@gmail.com>
2025-01-03 17:47:00 +0530

    Add : Added a logic for total price with tax

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,7 +10,9 @@ class Order extends Model
 {
     use HasFactory;
 
-    protected $appends = ['quantity'];
+    // protected $appends = ['quantity'];
+    protected $appends = ['total_price_with_tax'];
+
 
     protected $fillable = [
         'customer_name', 
@@ -22,8 +25,25 @@ class Order extends Model
         return $this->belongsToMany(Medicine::class)->withPivot('quantity');
     }
 
-    // Quantity attribute
-    public function getQuantityAttribute() {
-        return $this->attributes['quantity'];
+    // public function getQuantityAttribute()
+    // {
+    //     return $this->getAttribute('quantity');
+    // }
+    // public function getQuantityAttribute()
+    // {
+    //     // Calculate the total quantity of medicines in the order
+    //     return $this->medicines->sum('pivot.quantity');
+    // }
+    protected function totalPriceWithTax(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->calculateTotalPriceWithTax(),
+        );
     }
+
+    protected function calculateTotalPriceWithTax()
+    {
+        return $this->total_price * 1.1; // 10% tax rate
+    }
+
 }
